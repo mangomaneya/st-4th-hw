abf2966f04096690840b603baa802b59ea90b5b8
lee kiri <mango.man.e.ya@gmail.com>
2025-02-01 01:27:40 +0900

    디테일 페이지에 라우터 파라미터 추가 및 홈 페이지에 링크 추가, 레이아웃 컴포넌트 생성

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,8 +1,9 @@
+import { useParams } from "react-router-dom";
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
-}
+  const param = useParams();
+  console.log("path parameter 로 받은 id =>", param.id);
+  return <div>Detail</div>;
+};
 
-export default Detail
+export default Detail;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
+import { Link, useLocation } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  // const navigate = useNavigate(); //버튼에 navigate("url주소")로 활용 가능
+  const location = useLocation();
+  // console.log('navigate', navigate);
+  console.log("location", location); // 현재 pathname확인 가능
 
-export default Home
+  return (
+    <div>
+      Home
+      <Link to="/detail">디테일로 이동</Link>
+    </div>
+  );
+};
+
+export default Home;
